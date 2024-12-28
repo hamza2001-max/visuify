e1900f1736ece4738a6dbbd546e46a4011502a81
Ahmmmyoo <ahmadirfankhan@gmail.com>
2024-12-28 14:45:55 +0500

    update

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,14 @@
 const Login = () => {
   return (
-    <div>Login</div>
+    <section className="my-10 py-10 flex justify-center xl:shadow-lg xl:rounded-lg">
+      <div className="flex flex-col items-center space-y-4">
+        <div className="space-y-4 mb-4">
+
+          <div className="text-3xl font-medium">Login</div>
+        </div>
+      </div>
+    </section>
+
   )
 }
 
