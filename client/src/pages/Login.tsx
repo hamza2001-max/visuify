7901fbbeb5ba1316c9b5a358c702e758d1069ad6
Ahmmmyoo <ahmadirfankhan@gmail.com>
2024-12-29 22:22:47 +0500

    rough update to loginPage

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,41 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  IconBrandGithub,
+  IconBrandGoogle,
+  IconEye,
+  IconEyeOff,
+} from "@tabler/icons-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+const signupSchema = z.object({
+  username: z
+    .string()
+    .min(3, "Username must be at least 3 characters")
+    .max(20, "Username must be at most 20 characters"),
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(50, "Password must be at most 50 characters"),
+});
+
+type FormSchema = z.infer<typeof signupSchema>;
+
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchema>({
+    resolver: zodResolver(signupSchema),
+  });
+  const onSubmit = (data: FormSchema) => {
+    console.log(data);
+  };
   return (
     <section className="my-10 py-10 flex justify-center xl:shadow-xl xl:rounded-lg">
       <div className="flex flex-col items-center space-y-4">
@@ -28,12 +63,28 @@ const Login = () => {
             </label>
             <div className="relative">
               <input
-                type="password"
+                type={!showPassword ? "password" : "text"}
                 id="password"
                 placeholder="Enter password..."
+                {...register("password")}
                 className="border border-1 py-3 pl-2 pr-10 rounded-md outline-none w-full"
               />
+              {!showPassword && (
+                <IconEye
+                  className="absolute top-[14px] right-3 cursor-pointer"
+                  onClick={() => setShowPassword(true)}
+                />
+              )}
+              {showPassword && (
+                <IconEyeOff
+                  className="absolute top-[14px] right-3 cursor-pointer"
+                  onClick={() => setShowPassword(false)}
+                />
+              )}
             </div>
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
           </div>
           <button
             type="submit"
