6fdf7a31121306fbfdb5d14054dfb7b88877b156
Ahmmmyoo <ahmadirfankhan@gmail.com>
2024-12-29 15:02:39 +0500

    added logon page and a home page, the home page is not even bare minimum

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Navbar from "./components/include/Navbar";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <div className="container">
         <Navbar />
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/" element={<Home />} />
           <Route path="/imagestudio" element={<ImageStudio />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,11 +1,48 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
-    <section className="my-10 py-10 flex justify-center xl:shadow-lg xl:rounded-lg">
+    <section className="my-10 py-10 flex justify-center xl:shadow-xl xl:rounded-lg">
       <div className="flex flex-col items-center space-y-4">
         <div className="space-y-4 mb-4">
-
           <div className="text-3xl font-medium">Login</div>
+          <p>Enter your details below to login to your account.</p>
         </div>
+        <form
+          className="flex flex-col space-y-4 w-full !mt-10"
+        >
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="username" className="w-fit">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              placeholder="Enter username..."
+              className="border border-1 py-3 px-2 rounded-md outline-none"
+            />
+          </div>
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="password" className="w-fit">
+              Password
+            </label>
+            <div className="relative">
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter password..."
+                className="border border-1 py-3 pl-2 pr-10 rounded-md outline-none w-full"
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="shadow-sm !mt-10 font-semibold text-lg border-2 bg-black text-white hover:bg-white hover:text-black transition-colors duration-200 border-black px-2 py-3 rounded-md"
+          >
+            Login
+          </button>
+          <p className="text-center">Don't have an account? <Link to="/signup" className="font-medium">SignUp</Link></p>
+        </form>
       </div>
     </section>
 
