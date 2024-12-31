c0e155c8f0fabf19cdeeaa6b1c9f6deff8555946
hamza2001-max <ellawliet1007@gmail.com>
2025-01-01 00:23:11 +0500

    added the dockers setting for db

--- server/app/db/database.py
+++ server/app/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql+psycopg2://openpg:openpgpwd@localhost:5432/visuify'
+DATABASE_URL = 'postgresql+psycopg2://postgres:postgres@db:5432/visuify_db'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
