6a1264447950726f79e4eba09a31ed27bce09363
Abbas Khan <abbaskhhann0@gmail.com>
2024-12-31 20:00:53 +0500

    created sign-up and login api

---
+++ server/app/db/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = 'postgresql+psycopg2://openpg:openpgpwd@localhost:5432/visuify'
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- server/app/main.py
+++ server/app/main.py
@@ -1,15 +1,32 @@
-from typing import Union
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from .db.database import Base, engine, get_db
+from .models.models import User
+from .utils.auth import hash_password, create_access_token
 
-from fastapi import FastAPI
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+@app.post("/signup/")
+def sign_up(username: str, email: str, password: str, db: Session = Depends(get_db)):
+    user_exists = db.query(User).filter(User.email == email).first()
+    if user_exists:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    hashed_password = hash_password(password)
+    new_user = User(username=username, email=email, hashed_password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    return {"message": "User created successfully"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/login/")
+def login(email: str, password: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == email).first()
+    if not user or not hash_password(password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    
+    access_token = create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
